30003edde36a4c2ccd8e41f437df20bd78fea2c6
VikramP7 <vikram.procter@gmail.com>
2025-03-10 13:12:21 -0600

    No Clue what i changed

--- firmware_common/application/user_app1.c
+++ firmware_common/application/user_app1.c
@@ -305,7 +305,7 @@ static void UserApp1SM_ChannelOpen()
         sStringLocation.u16PixelColumnAddress = U16_LCD_CENTER_COLUMN - (strlen((char const *)au8DataContent) * (U8_LCD_SMALL_FONT_COLUMNS + U8_LCD_SMALL_FONT_SPACE) / 2);
         sStringLocation.u16PixelRowAddress = U8_LCD_SMALL_FONT_LINE7;
 
-                LcdClearPixels(&G_sLcdClearLine7);
+        LcdClearPixels(&G_sLcdClearLine7);
         LcdLoadString(&au8DataContent, LCD_FONT_SMALL, &sStringLocation);
 
         // update our local message counter and send the message back
